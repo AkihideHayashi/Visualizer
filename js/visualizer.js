e10676d3c25241b27a296e4a8a3a4e317adb4539
Akihide Hayashi <akihide@d042-030.p042.chem.sci.osaka-u.ac.jp>
2019-05-09 19:37:19 +0900

    save

--- js/visualizer.js
+++ js/visualizer.js
@@ -1,32 +1,5 @@
 // import { log } from "winjs";
 
-function init_visualizer(canvas) {
-    objects = init_objects(canvas);
-    canvas.addEventListener('drop', read_file(objects));
-    window.addEventListener('resize', ()=>onResize(objects));
-    onResize(objects);
-    tick(objects);
-}
-
-function init_objects(canvas){
-    const renderer = new THREE.WebGLRenderer({
-        canvas: canvas,
-        antialias: true
-    });
-
-    let scene = new THREE.Scene();
-    let camera = new THREE.PerspectiveCamera(45, 1.0);
-    let light = new THREE.PointLight(0xffffff);
-    let raycaster = new THREE.Raycaster();
-    let atoms = [];
-    let n = 0;
-
-    // scene.add(light);
-    let objects = {canvas: canvas, renderer: renderer, scene: scene, camera: camera, light: light, raycaster: raycaster, atoms: atoms, meshes: [], n:n};
-    const controls = new OrbitControls(objects);
-    return objects;
-}
-
 const color = {
     H: 0xffffff,
     Pt: 0xa0a0a0,
@@ -37,34 +10,135 @@ const radius = {
     Pt: 1.39,
 }
 
-function draw_atoms(objects){
-    const {scene, meshes, n} = objects;
-    env = objects.atoms[n];
-    while(meshes.length > 0){
-        meshes.pop(0);
-    }
-    while(scene.children.length > 0){ 
-        scene.remove(scene.children[0]); 
-    }
-    scene.add(objects.light);
-    for (const atom of env.atoms){
-        mesh = new THREE.Mesh(
-            new THREE.SphereGeometry( radius[atom.n], 32, 32 ),
-            new THREE.MeshLambertMaterial({ color: color[atom.n] })
-        );
-        mesh.position.set(atom.x, atom.y, atom.z);
-        scene.add(mesh);
-        meshes.push(mesh);
-    }
-    if (env.cell){
-        const lattice = [
-            new THREE.Vector3(env.cell[0][0], env.cell[0][1], env.cell[0][2]),
-            new THREE.Vector3(env.cell[1][0], env.cell[1][1], env.cell[1][2]),
-            new THREE.Vector3(env.cell[2][0], env.cell[2][1], env.cell[2][2])
-        ];
-        draw_lattice(scene, lattice);
-    }
+function init_visualizer(canvas) {
+    let objects = new Visualizer(canvas);
+    canvas.addEventListener('drop', objects.read_file());
+    window.addEventListener('resize', ()=>onResize(objects));
+    onResize(objects);
+    tick(objects);
 }
+
+class Visualizer {
+    constructor(canvas, input) {
+        this.canvas = canvas;
+        this.input = input;
+        this.renderer = new THREE.WebGLRenderer({
+            canvas: canvas,
+            antialias: true,
+        })
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(45, 1.0);
+        this.camera_light = new THREE.PointLight(0xffffff);
+        this.raycaster = new THREE.Raycaster();
+        this.meshes = [];
+        this.input = [];
+        this.index = 0;
+        this.controls = new OrbitControls(this);
+        console.log(this);
+    }
+
+    read_file(){
+        let self = this;
+        function inner(event){
+            event.preventDefault();
+            const reader = new FileReader();
+            reader.readAsText(event.dataTransfer.files[0]);
+            reader.addEventListener('load', function(e){
+                self.input = JSON.parse(reader.result);
+                self.index = 0;
+                self.draw_atoms();
+            })
+        }
+        return inner;
+    }
+
+    draw_atoms(){
+        const {scene, meshes, index} = this;
+        let env = this.input[index];
+        while(meshes.length > 0){
+            meshes.pop(0);
+        }
+        while(scene.children.length > 0){ 
+            scene.remove(scene.children[0]); 
+        }
+        scene.add(this.camera_light);
+        for (const atom of env.atoms){
+            let mesh = new THREE.Mesh(
+                new THREE.SphereGeometry( radius[atom.n], 32, 32 ),
+                new THREE.MeshLambertMaterial({ color: color[atom.n] })
+            );
+            mesh.position.set(atom.x, atom.y, atom.z);
+            scene.add(mesh);
+            meshes.push(mesh);
+        }
+        if (env.cell){
+            const lattice = [
+                new THREE.Vector3(env.cell[0][0], env.cell[0][1], env.cell[0][2]),
+                new THREE.Vector3(env.cell[1][0], env.cell[1][1], env.cell[1][2]),
+                new THREE.Vector3(env.cell[2][0], env.cell[2][1], env.cell[2][2])
+            ];
+            draw_lattice(scene, lattice);
+        }
+    }
+    
+}
+
+// function read_file(objects){
+//     return function(e){
+//         const reader = new FileReader();
+//         reader.readAsText(e.dataTransfer.files[0]);
+//         reader.addEventListener('load', function(){
+//             objects.input = JSON.parse(reader.result);
+//             objects.n = 0;
+//             draw_atoms(objects);
+//         })
+//         e.preventDefault();
+//     }
+// }
+
+function tick(objects){
+    objects.renderer.render(objects.scene, objects.camera);
+    requestAnimationFrame(()=>tick(objects));
+}
+
+function onResize(objects){
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    objects.renderer.setPixelRatio(window.devicePixelRatio);
+    objects.renderer.setSize(width, height);
+    objects.camera.aspect = width / height;
+    objects.camera.updateProjectionMatrix();
+}
+
+
+// function draw_atoms(objects){
+//     const {scene, meshes, index} = objects;
+//     env = objects.input[index];
+//     while(meshes.length > 0){
+//         meshes.pop(0);
+//     }
+//     while(scene.children.length > 0){ 
+//         scene.remove(scene.children[0]); 
+//     }
+//     scene.add(objects.camera_light);
+//     for (const atom of env.atoms){
+//         mesh = new THREE.Mesh(
+//             new THREE.SphereGeometry( radius[atom.n], 32, 32 ),
+//             new THREE.MeshLambertMaterial({ color: color[atom.n] })
+//         );
+//         mesh.position.set(atom.x, atom.y, atom.z);
+//         scene.add(mesh);
+//         meshes.push(mesh);
+//     }
+//     if (env.cell){
+//         const lattice = [
+//             new THREE.Vector3(env.cell[0][0], env.cell[0][1], env.cell[0][2]),
+//             new THREE.Vector3(env.cell[1][0], env.cell[1][1], env.cell[1][2]),
+//             new THREE.Vector3(env.cell[2][0], env.cell[2][1], env.cell[2][2])
+//         ];
+//         draw_lattice(scene, lattice);
+//     }
+// }
 
 function draw_lattice(scene, lattice){
     z = new THREE.Vector3(0.0, 0.0, 0.0);
@@ -87,48 +161,22 @@ function draw_lattice(scene, lattice){
     scene.add(arrow(l01, l012, 0xffffff, 0.01, 0.01));
     scene.add(arrow(l12, l012, 0xffffff, 0.01, 0.01));
     scene.add(arrow(l02, l012, 0xffffff, 0.01, 0.01));
-}
-
-function arrow(start, end, color, headlength, headwidth){
-    let v = end.clone().sub(start);
-    let l = v.length();
-    let n = v.clone().normalize();
-    return new THREE.ArrowHelper(n, start, l, color, headlength, headwidth);
-}
-
-
-function read_file(objects){
-    return function(e){
-        const reader = new FileReader();
-        reader.readAsText(e.dataTransfer.files[0]);
-        reader.addEventListener('load', function(){
-            objects.atoms = JSON.parse(reader.result);
-            objects.n = 0;
-            draw_atoms(objects);
-        })
-        e.preventDefault();
+    function arrow(start, end, color, headlength, headwidth){
+        let v = end.clone().sub(start);
+        let l = v.length();
+        let n = v.clone().normalize();
+        return new THREE.ArrowHelper(n, start, l, color, headlength, headwidth);
     }
 }
 
-function tick(objects){
-    objects.renderer.render(objects.scene, objects.camera);
-    requestAnimationFrame(()=>tick(objects));
-}
 
-function onResize(objects){
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    objects.renderer.setPixelRatio(window.devicePixelRatio);
-    objects.renderer.setSize(width, height);
-    objects.camera.aspect = width / height;
-    objects.camera.updateProjectionMatrix();
-}
+
 
 OrbitControls = function ( objects ) {
     const {light, camera, raycaster, meshes} = objects;
     camera.position.set(0, 0, +100);
-    objects.light.position.copy(camera.position);
-    objects.scene.add(objects.light);
+    objects.camera_light.position.copy(camera.position);
+    objects.scene.add(objects.camera_light);
     let look = new THREE.Vector3(0.0, 0.0, 0.0).sub(camera.position);
     let mouse = new THREE.Vector2(0.0, 0.0);
     let mouse_is_down = false;
@@ -136,7 +184,7 @@ OrbitControls = function ( objects ) {
 
 
     function update_light(){
-        objects.light.position.copy(camera.position);
+        objects.camera_light.position.copy(camera.position);
     }
 
     function onwheel(event){
@@ -223,21 +271,21 @@ OrbitControls = function ( objects ) {
         set_mouse_position(event);
     }
 
-    function oncontextmenu(event){
-        let atoms = objects.atoms;
-        for(i in atoms){
-            v = meshes[i].position;
-            atoms[i].x = v.x;
-            atoms[i].y = v.y;
-            atoms[i].z = v.z;
-        }
-        console.log(atoms);
-        let blob = new Blob([JSON.stringify([{atoms: atoms}])], {"type": "text/plain"});
-        // document.getElementById("download").href = window.URL.createObjectURL(blob);
-        saveAs(blob, "ouput.json")
-        event.preventDefault();
-        event.stopPropagation();
-    }
+    // function oncontextmenu(event){
+    //     let input = objects.input;
+    //     for(i in input){
+    //         v = meshes[i].position;
+    //         atoms[i].x = v.x;
+    //         atoms[i].y = v.y;
+    //         atoms[i].z = v.z;
+    //     }
+    //     console.log(atoms);
+    //     let blob = new Blob([JSON.stringify([{atoms: atoms}])], {"type": "text/plain"});
+    //     // document.getElementById("download").href = window.URL.createObjectURL(blob);
+    //     saveAs(blob, "ouput.json")
+    //     event.preventDefault();
+    //     event.stopPropagation();
+    // }
 
     objects.canvas.addEventListener('wheel', onwheel, false);
     objects.canvas.addEventListener('mousedown', onmousedown, false);
